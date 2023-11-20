6428e589772fbd8b3c97cdaf06ff7240d089c776
Gustavo Henrique <gustavohenriqueprivado@gmail.com>
2023-11-20 19:14:41 -0300

    fix: key prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -307,7 +307,10 @@ export default function Home() {
 
     const socialMediaComponent = Object.keys(sociaMedia).map((key) => {
         return (
-            <div className="flex flex-col items-center w-1/2 overflow-hidden hover:scale-105 transition-all duration-200 ease-in-out mt-12 gap-4">
+            <div
+                className="flex flex-col items-center w-1/2 overflow-hidden hover:scale-105 transition-all duration-200 ease-in-out mt-12 gap-4"
+                key={key}
+            >
                 <h1 className="font-light text-xl">
                     {sociaMedia[key as keyof typeof sociaMedia].name}
                 </h1>
